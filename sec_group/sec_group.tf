f5653b608e930e9fafa3d8a57ea10334ab4382e1
srdrkn <serdar.akin@yahoo.com>
2021-08-03 03:02:01 +0000

    added sec_group

---
+++ sec_group/sec_group.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "external_by_terraform" {
+  name        = "external_by_terraform"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-077fb4c22542ec3d4"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["50.194.68.230/32"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["50.194.68.231/32"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
